e82979469c7548f3504c5d970c406017de3d294c
Jeremy Mayer <28955136+subcurmudgeon@users.noreply.github.com>
2021-03-02 11:11:30 -0500

    Update publi.sh

--- publi.sh
+++ publi.sh
@@ -17,10 +17,10 @@ PANDOC+=(--to="html5")
 PANDOC+=(--standalone)
 
 # -- Optional Pandoc Files
-[ -f "$1/.publi.sh/include/in-header.html" ] && PANDOC+=(--include-in-header="$1/.publi.sh/include/in-header.html")
-[ -f "$1/.publi.sh/include/before-body.html" ] && PANDOC+=(--include-before-body="$1/.publi.sh/include/before-body.html")
-[ -f "$1/.publi.sh/include/after-body.html" ] && PANDOC+=(--include-after-body="$1/.publi.sh/include/after-body.html")
-[ -f "$1/.publi.sh/template/publi.sh.html5" ] && PANDOC+=(--template="$1/.publi.sh/template/publi.sh.html5")
+[[ -f "$1"/.publi.sh/include/in-header.html ]] && PANDOC+=(--include-in-header="$1/.publi.sh/include/in-header.html")
+[[ -f "$1"/.publi.sh/include/before-body.html ]] && PANDOC+=(--include-before-body="$1/.publi.sh/include/before-body.html")
+[[ -f "$1"/.publi.sh/include/after-body.html ]] && PANDOC+=(--include-after-body="$1/.publi.sh/include/after-body.html")
+[[ -f "$1"/.publi.sh/template/publi.sh.html5 ]] && PANDOC+=(--template="$1/.publi.sh/template/publi.sh.html5")
 
 # -- Common Message Array
 declare -A MSG=(
