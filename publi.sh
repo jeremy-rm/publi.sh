5c3e9849cabfd1b0b024cd8c56689bff64924dae
Jeremy Mayer <28955136+subcurmudgeon@users.noreply.github.com>
2021-03-03 14:22:41 -0500

    Update publi.sh

--- publi.sh
+++ publi.sh
@@ -69,22 +69,23 @@ publish_init() {
 		publish_die 2 "invalid number of arguments, use $0 -h for help"
 	fi
 
+	# Sanity: Check that destination directory exists, or create it.
+#	if ! [[ -d "$2" ]]
+#	then
+#		mkdir -p "$2" || publish_die 2 "cannot create destination directory: $2"
+#		publish_debug "destination directory created: $2"
+#	fi
+
+	# Sanity: Check that destination directory exists and is writable.
+#	if ! [[ -w "$2" ]]
+#	then
+#		publish_die 2 "destination directory is not writable or does not exist: $2"
+#	fi
+
 	# Sanity: Check that source directory exists and is readable.
 	if ! [[ -r "$1" ]]
 	then
 		publish_die 2 "source directory is not readable or does not exist: $1"
-	fi
-
-	# Sanity: Check that destination directory exists, or create it.
-	if ! [[ -d "$2" ]]
-	then
-		mkdir -p "$2" || publish_die 2 "cannot create destination directory: $2"
-	fi
-
-	# Sanity: Check that destination directory exists and is writable.
-	if ! [[ -w "$2" ]]
-	then
-		publish_die 2 "destination directory is not writable or does not exist: $2"
 	fi
 
 	# Safety: Check that destination directory is empty to prevent accidental overwrites.
